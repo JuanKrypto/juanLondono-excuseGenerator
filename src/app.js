da2d678082551b3bf44d08bb9e1b1718f52901c3
J. Krypto <117060264+JuanKrypto@users.noreply.github.com>
2024-02-06 21:59:54 +0000

    JC

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let quien = ["pepe", "pepa", "pipe", "papi"];
+  let cuando = ["mañana", "medio dia", "tarde", "noche"];
+  let accion = ["pepe", "pepa", "pipe", "papi"];
+  let que = ["pepe", "pepa", "pipe", "papi"];
+
+  var palabraQuien = quien[Math.floor(Math.random() * quien.length)];
+  var palabraCuando = cuando[Math.floor(Math.random() * cuando.length)];
+  var palabraAccion = accion[Math.floor(Math.random() * accion.length)];
+  var palabraQue = que[Math.floor(Math.random() * que.length)];
+
+  let unirPalabras =
+    palabraQuien + " " + palabraCuando + " " + palabraAccion + " " + palabraQue;
+
+  let parrafo = document.querySelector("#excusas");
+  parrafo.textContent = unirPalabras;
+
+  console.log(unirPalabras);
 };
